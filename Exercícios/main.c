efa6ecadccd2432996bca288f23a66b2ec994e4d
Lucas Gabriel <104915568+aflucasgabriel@users.noreply.github.com>
2023-06-29 16:33:43 -0300

    IMC

--- Exercícios/main.c
+++ Exercícios/main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
-   int a, b, c;
-    printf("a b c: "); scanf(" %d %d %d", &a,&b,&c);
-    printf("\nResolvendo equacao %dx^2 + %dx + %d = 0\n\n",a,b,c);
-    if (a == 0) {
-        printf("nao eh eq 2o grau."); return 0;
-    }
-    float delta = b * b - 4 * a * c;
-    if (delta == 0) {
-        float x = - b / (2 * a);
-        printf("Raiz %f\n\n", x);
-    }
-    else if (delta > 0) {
-        float x1 = (-b + sqrt(delta)) / (2 * a);
-        float x2 = (-b - sqrt(delta)) / (2 * a);
-        printf("Raizes %f %f\n\n", x1, x2);
-    }
-    else {
-        printf("Nao tem raizes reais.\n\n");
-    }
+    char sexo;
+    printf("digite seu sexo (m ou f): ");
+    sexo = getchar();
+
+    float altura, ideal;
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
+
+    if ((sexo == 'F') || (sexo == 'f'))
+        ideal = 62.1 * altura - 44.7;
+
+    else if ((sexo == 'M')|| (sexo == 'm'))
+        ideal = 72.7 * altura - 58;
+        else {
+            printf("Opcao invalida.");
+            return 0;
+        }
+
+        printf("Sexo %c altura %.2f peso ideal %.2f", sexo, altura, ideal);
+
+    return 0;
 }
